ee2c6b439ff3d5a791e0b36ebdce21c61272c05f
shadowfox3735 <normanpan180@gmail.com>
2019-04-15 23:39:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,6 +20,12 @@ class InteractiveRecord
     column_names.compact
   end
   
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
   
   
 end
