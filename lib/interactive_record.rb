dc09014ff41a8f6e57153be687518e4f7c6c9290
shadowfox3735 <normanpan180@gmail.com>
2019-04-16 00:10:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end
   
   def self.find_by(key)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{key}'"
     DB[:conn].execute(sql)
   end
   
